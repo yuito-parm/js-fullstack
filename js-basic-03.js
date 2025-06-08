c2677e0af0ea952ec4cfaa4f1baa3713d92900f2
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-08 22:11:49 +0900

    JS基礎：for文とif文の練習

---
+++ js-basic-03.js
@@ -0,0 +1,8 @@
+let animals = ["ハリネズミ", "フクロウ", "フェネック"];
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+    if (animals[i] === "ハリネズミ") {
+        console.log("かわいいハリネズミだよ！");
+    }
+}
