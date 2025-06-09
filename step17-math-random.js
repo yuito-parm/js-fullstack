a59b189957f3874a41c3b1c9abcd78d4f486a030
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-09 15:42:41 +0900

    JS：Mathで乱数・四捨五入・最大/最小値

---
+++ step17-math-random.js
@@ -0,0 +1,11 @@
+const rand = Math.floor(Math.random() * 100) + 1;
+console.log(rand);
+
+const n = 10.7;
+console.log(Math.round(n));
+console.log(Math.ceil(n));
+console.log(Math.floor(n));
+
+const nums = [4, 8, 1, 23, 15];
+console.log(Math.max(...nums));
+console.log(Math.min(...nums));
