bd55e37c2e1717b7c91a2c51452bea484775b115
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-10 19:32:58 +0900

    .thenで繋げる方法も記載

--- step30-fetch-api.js
+++ step30-fetch-api.js
@@ -1,11 +1,21 @@
-async function fetchusers() {
+// thenチェーンで書く場合
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+    console.log(data); // ユーザー配列
+    })
+    .catch(err => {
+    console.error("通信エラー", err);
+    });
+
+// async/awaitで書く場合（関数の中で）
+async function fetchUsers() {
     try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/users");
-        const data = await res.json();
-        console.log(data[0]);
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    console.log(data); // ユーザー配列
     } catch (err) {
-        console.error("通信エラー", err);
+    console.error("通信エラー", err);
     }
 }
-
-fetchusers();
+fetchUsers();
