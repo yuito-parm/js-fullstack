ba318a01d3ac5e97ee758b6d9dffcc91b6dda2aa
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-10 19:09:50 +0900

    JS：fetchでWeb APIからデータ取得

--- step30-fetch-api.js
+++ step30-fetch-api.js
@@ -2,7 +2,7 @@ async function fetchusers() {
     try {
         const res = await fetch("https://jsonplaceholder.typicode.com/users");
         const data = await res.json();
-        console.log(data);
+        console.log(data[0]);
     } catch (err) {
         console.error("通信エラー", err);
     }
