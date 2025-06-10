f74f81e566b41a9af7e65f8ac1cf864a8c8abdc7
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-10 15:18:03 +0900

    JS：リスト要素のダブルクリック編集機能

---
+++ step28-dom-edit.js
@@ -0,0 +1,30 @@
+document.getElementById("inputBtn").onclick = function() {
+    const input = document.getElementById("animalInput");
+    const text = input.value.trim();
+    if (text) {
+        const li = document.createElement("li");
+        li.textContent = text;
+
+        li.onclick = function() {
+            li.remove();
+        };
+        li.ondblclick = function() {
+            const currentText = li.textContent;
+            const editInput = document.createElement("input");
+            editInput.tyoe = "text";
+            editInput.value = currentText;
+            li.textContent = "";
+            li.appendChild(editInput);
+            editInput.focus();
+
+            editInput.onblur = editInput.onkeydown = function(e) {
+                if (!e || e.type === "blur" || (e.key === "Enter")) {
+                    const newText = editInput.value.trim();
+                    li.textContent = newText ? newText : currentText;
+                }
+            };
+        };
+        document.getElementById("animalList").appendChild(li);
+        input.value = "";
+    }
+};
